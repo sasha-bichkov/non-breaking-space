d1242b4adfade141e1b7007dd2db861814df4df9
sasha-bichkov <aleksandar.bichkov@gmail.com>
2016-02-10 17:47:45 +0300

    release 0.0.1

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 'use strict';
-module.exports = function () { return nbsp; };
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(options) {
+  var options = options || {};
+  var lang = 'ru';
+  var nbsp = '&nbsp;';
+
+  var prepositions = {
+    ru: /\s(я|к|с|в|(д|т|к|з)?ак?|(д|п)о|и(з|х)?|о(б|т|н)?|уж?|н(а|о|е)|вы)\s/gi
+  };
+
+  var replaceNbsp = function(text) {
+    return text.replace(prepositions[lang], function(match) {
+      return match.trimRight() + nbsp;
+    });
+  }
+  if (options.i == null && options.s == null) {
+    return new TypeError("Error#options: the `input` parameter isn't specified");
+  }
+
+  if (options.l != null) {
+    var lang = options.l;
+  }
+
+  if (options.i) {
+    var inputPath = path.resolve(__dirname, options.i);
+    var file = fs.readFileSync(inputPath, 'utf8');
+    return replaceNbsp(file);
+  }
+
+  if (options.s) {
+    return replaceNbsp(options.s);
+  }
+}
--- test.js
+++ test.js
@@ -1,2 +1,39 @@
 import test from 'ava';
 import nbsp from './';
+
+var nbspCount = function(text) {
+  return text.match(/&nbsp;/g).length;
+};
+
+test(t => {
+  const not_exists = nbsp();
+  t.is(not_exists.message, "Error#options: the `input` parameter isn't specified");
+});
+
+test(t => {
+  // lang: ru, only string
+  const s1 = 'Шла Саша по шоссе и сосала сушку';
+  const s2 = 'Ребята не стоит вскрывать этот код. Вы молодые, шутливые, вам все легко.';
+  const s3 = 'Впрочем, на этот раз страх встречи с своею кредиторшей даже его самого поразил по выходе на улицу.';
+
+  const res1 = nbsp({ s: s1, l: 'ru' });
+  const res2 = nbsp({ s: s2, l: 'ru' });
+  const res3 = nbsp({ s: s3, l: 'ru' });
+
+  const count1 = nbspCount(res1);
+  const count2 = nbspCount(res2);
+  const count3 = nbspCount(res3);
+
+  t.is(count1, 2);
+  t.is(count2, 2);
+  t.is(count3, 4);
+});
+
+test(t => {
+  // lang: ru, file...
+  const res1 = nbsp({ i: 'input.txt', l: 'ru' });
+
+  const count1 = nbspCount(res1);
+
+  t.is(count1, 12);
+});
